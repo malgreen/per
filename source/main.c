b72d6fea637ff3bc41d68dca228c5473236c0623
Martin Algreen <git@mal.green>
2025-02-10 09:34:27 +0100

    sdl3 static linked

--- source/main.c
+++ source/main.c
@@ -1,8 +1,5 @@
 
-#include "raylib.h"
-
-#define RAYGUI_IMPLEMENTATION
-#include "raygui.h"
+#include <SDL3/SDL.h>
 
 typedef struct APP_STATE
 {
@@ -16,59 +13,6 @@ typedef struct APP_STATE
 //------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-
-    // State
-    APP_STATE appState = {
-        .screenWidth = 800,
-        .screenHeight = 450,
-    };
-    APP_STATE prevAppState; // TODO: should we allocate memory?
-
-    // Window
-    InitWindow(appState.screenWidth, appState.screenHeight, "Per");
-    SetWindowState(FLAG_WINDOW_RESIZABLE);
-
-    // Refresh rate
-    int monitorRefreshRate = GetMonitorRefreshRate(GetCurrentMonitor()); // TODO: Change when changing monitor
-    SetTargetFPS(monitorRefreshRate);
-
-    //--------------------------------------------------------------------------------------
-
-    // Main Loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
-    {
-        // UPDATE
-        //----------------------------------------------------------------------------------
-        appState.screenWidth = GetScreenWidth();
-        appState.screenHeight = GetScreenHeight();
-
-        //----------------------------------------------------------------------------------
-
-        // RENDER
-        //----------------------------------------------------------------------------------
-        // TODO: retained-mode experimentation, just freezes the program right now?
-        if (&prevAppState != NULL && (&appState, &prevAppState, sizeof(appState)) == 0)
-        {
-            continue;
-        }
-        prevAppState = appState; // copies memory
-
-        BeginDrawing();
-
-        ClearBackground(WHITE);
-
-        DrawText("text goes here", appState.screenWidth / 2, appState.screenHeight / 2, 20, BLACK);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow(); // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    // printf("Hello World\n");
     return 0;
 }
